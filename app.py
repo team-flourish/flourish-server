8ff8a244afecf45e5a46b8c7f9c3ac9e5bd98a8c
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-04-01 18:28:02 +0100

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from config import Config
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -6,6 +7,7 @@ from werkzeug import exceptions
 from controllers import users
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 CORS(app)
 
 # database setup
@@ -37,15 +39,57 @@ class Products(db.Model):
     description = db.Column(db.String(100))
     image = db.Column(db.String(500))
 
+    def __str__(self):
+        return f"{self.product_id}"
+
 class Category(db.Model):
     __tablename__ = 'category'
     category_id = db.Column(db.Integer, primary_key=True)
     category_name  = db.Column(db.String(100))
-
+db.create_all()
 @app.get('/')
 def index():
   return 'Hello, world!'
 
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    if request.method == 'GET':
+
+        try: 
+            allProducts = Products.query.all()
+            print(allProducts)
+            return allProducts
+
+        except exceptions.NotFound:
+            raise exceptions.NotFound("There are no products in the database currently!")
+
+        except:
+            raise exceptions.InternalServerError()
+
+
+    elif request.method == 'POST':
+    # format of request { description, category_id, is_retail, location, price, expiry, image} 
+    # { "user_id": 1, "description": "Tomatoes", "category_id": 2, "is_retail": "True", "location": "SE18", "price": 2.99, "expiry": "02/04/2022", "image": "LINK"}
+        
+        req = request.json
+        try:
+            new_product = Products(
+                user_id = req.user_id, 
+                description = req.description, 
+                category_id = req.category_id,
+                is_Retail = req.is_retail, 
+                location = req.location, 
+                price = req.price, 
+                expiry = req.expiry, 
+                image = req.image
+            )
+
+            db.session.add(new_product)
+            db.session.commit()
+            return f"New product was added!", 201
+
+        except: 
+            raise exceptions.InternalServerError()
 
 
 # boiler plate
--- zahra.py
+++ zahra.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -40,4 +40,49 @@ class Category(db.Model):
     __tablename__ = 'category'
     category_id = db.Column(db.Integer, primary_key=True)
     category_name  = db.Column(db.String(100))
-   
+
+
+@app.get('/')
+def index():
+  return 'Hello, world!'
+
+
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    if request.method == 'GET':
+
+        try: 
+            allProducts = Products.query.all()
+            return jsonify(allProducts), 200
+
+        except exceptions.NotFound:
+            raise exceptions.NotFound("There are no products in the database currently!")
+
+        except:
+            raise exceptions.InternalServerError()
+
+
+    elif request.method == 'POST':
+    # format of request { description, category_id, is_retail, location, price, expiry, image} 
+    # { "user_id": 1, "description": "Tomatoes", "category_id": 2, "is_retail": "True", "location": "SE18", 
+    # "price": 2.99, "expiry": "02/04/2022", "image": "LINK"}
+        
+        req = request.json
+        try:
+            new_product = Products(
+                user_id = req.user_id, 
+                description = req.description, 
+                category_id = req.category_id,
+                is_retail = req.is_retail, 
+                location = req.location, 
+                price = req.price, 
+                expiry = req.expiry, 
+                image = req.image
+            )
+
+            db.session.add(new_product)
+            db.session.commit()
+            return f"New product was added!", 201
+
+        except: 
+            raise exceptions.InternalServerError()
