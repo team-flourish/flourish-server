086f5496cd9c3b6bc33773beaae8b8ddb72d19f3
bojin11 <15011278723@163.com>
2022-04-01 14:38:02 +0100

    bug fix

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug import exceptions
+from controllers import users
 
 app = Flask(__name__)
 CORS(app)
@@ -44,3 +45,44 @@ class Category(db.Model):
 @app.get('/')
 def index():
   return 'Hello, world!'
+
+@app.route('/users/<int:user_id>/rating', methods=['POST'])
+def rating_handler(user_id):
+    fns = {
+        'POST': users.createRating
+    }
+    resp, code = fns[request.method](request, user_id)
+    return jsonify(resp), code
+
+@app.route('/users/<int:user_id>', methods=['GET', 'DELETE'])
+def user_handler(user_id):
+    fns = {
+        'GET': users.show_user_details,
+        'DELETE': users.destroy_user
+    }
+    resp, code = fns[request.method](request, user_id)
+    return jsonify(resp), code
+
+@app.route('/users/<int:user_id>/location', methods=['PATCH'])
+def location_handler(user_id):
+    fns = {
+        'PATCH': users.update_location,
+    }
+    resp, code = fns[request.method](request, user_id)
+    return jsonify(resp), code
+
+@app.route('/users/<int:user_id>/product', methods=['GET'])
+def product_handler(user_id):
+    fns = {
+        'GET': users.get_products,
+    }
+    resp, code = fns[request.method](request, user_id)
+    return jsonify(resp), code
+
+@app.route('/users/<int:user_id>/radius', methods=['PATCH'])
+def radius_handler(user_id):
+    fns = {
+        'PATCH': users.update_radius,
+    }
+    resp, code = fns[request.method](request, user_id)
+    return jsonify(resp), code
