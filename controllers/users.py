9e7cfaf4e6a6540b58d673a28d62392ffb2cc25b
bojin11 <15011278723@163.com>
2022-04-01 14:46:51 +0100

    change in controller

--- controllers/users.py
+++ controllers/users.py
@@ -1,6 +1,10 @@
 
 users = [
-    {'id': 1, 'username': 'Zelda', 'email': 'Zelda@gmail.com', 'password': 'FDSFDSFDS', 'rating': 4.5, 'num_of_rating': 2, 'location': "CR4 9TT", 'radius': 1.3 },
+    {'id': 1, 'username': 'Zelda', 'email': 'Zelda@gmail.com', 'password': 'FDSFDSFDS', 'rating': 4.5, 'num_of_rating': 2, 'location': "BN9 9TT", 'radius': 1.3 },
+]
+
+products = [
+    {'product_id': 1; 'user_id': 1; 'category_id': 1; 'is_Retail': false, 'location': 'CR4 3RU', 'price': 4.3, 'expiry': '2022-04-01', 'description': 'tomato is in discount', 'image': 'dfsdkjfsd'}
 ]
 
 def show_user_details(req, uid):
@@ -17,6 +21,12 @@ def find_by_uid(uid):
     except:
         raise BadRequest(f"We don't have that user with id {uid}!")
 
+def find_product_by_uid(uid):
+    try:
+        return next(product for product in products if product['user_id'] == uid)
+    except:
+        raise BadRequest(f"We don't have any product with user {uid}!")
+
 def createRating(req, uid):
     user = find_by_uid(uid)
     data = req.get_json()
@@ -26,8 +36,8 @@ def createRating(req, uid):
     return rating, 200
 
 def get_products(req, uid):
-    user = find_by_uid(uid)
-    return user, 200
+    product = find_product_by_uid(uid)
+    return product, 200
 
 
 def update_radius(req, uid):
