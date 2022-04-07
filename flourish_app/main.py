49ab30865b4add5343a24e57099e50b6795d3c86
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-04-07 09:44:22 +0100

    add username and user rating to product and user product routes

--- flourish_app/main.py
+++ flourish_app/main.py
@@ -10,6 +10,8 @@ from .models import Productratings, Products, Users, Category
 
 from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, verify_jwt_in_request, decode_token
 
+
+
 main = Blueprint('main', __name__) 
 CORS(main)
 
